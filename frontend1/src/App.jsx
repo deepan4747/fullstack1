7d60e03be4b633e069044dab598fd5f2bf6e65bc
deepan4747 <deepankp.22msc@kongu.edu>
2025-06-20 16:19:32 +0530

    Day4

--- frontend1/src/App.jsx
+++ frontend1/src/App.jsx
@@ -1,16 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 import Welcome from './Welcome'
+import Skills from './skill'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Service from './Pages/Service'
+import Navbar from './components/Navbar'
+import State from './hooks/State'
+import Form from './hooks/Form'
+import Effects from './hooks/Effects'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Welcome name='Deepan' country='India'/>
-      <h1>Hello World</h1>
+    <Navbar/>
+    <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='/About' element={<About/>}/>
+       <Route path='/Service' element={<Service/>}/>
+       <Route path='/state' element={<State/>}/>
+       <Route path='/form' element={<Form/>}/>
+       <Route path='/effect' element={<Effects/>}/>
+    </Routes>
+    {/* <Welcome name='Deepan' country='India'/> */}
+    {/* <Skills skill={['React','Node','Express','MongoDb']}/>   */}
+          {/* <h1>Hello World</h1> */}
+
     </>
   )
 }
